2c3657e3eda56d70f87a84ed22b4e8ce6a3763a8
caasi Huang <caasi@users.noreply.github.com>
2015-05-25 01:28:03 +0800

    practice

--- rust/literals.rs
+++ rust/literals.rs
@@ -1,17 +1,14 @@
 fn main() {
-    println!("1 + 2 = {}", 1u32 + 2);
+    let x = 1u8;
+    let y = 2u32;
+    let z = 3f32;
 
-    println!("1 - 2 = {}", 1i32 - 2);
+    let i = 1;
+    let f = 1.0;
 
-    println!("true AND false is {}", true && false);
-    println!("true OR false is {}", true || false);
-    println!("NOT true is {}", !true);
-
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    println!("1 << 5 is {}", 1u32 << 5);
-    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
-
-    println!("One million is written as {}", 1_000_000u32);
+    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
 }
