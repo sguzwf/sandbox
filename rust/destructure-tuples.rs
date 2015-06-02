0b61abd2b873338f57d5bdc9b30fc123edd3d80a
caasi Huang <caasi@users.noreply.github.com>
2015-06-03 00:04:19 +0800

    practice

---
+++ rust/destructure-tuples.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let pair = (0, -2);
+
+    println!("Tell me about {:?}", pair);
+    match pair {
+        (0, y) => println!("First is `0`, and `y` is `{:?}`", y),
+        (x, 0) => println!("`x` is `{:?}, and last is `0`", x),
+        _      => println!("It does't matter what they are"),
+    }
+}
