54ec23a188e5994841360b3f1de74e3907f7329e
caasi Huang <caasi.igd@gmail.com>
2016-04-08 07:34:46 +0800

    Mimic

--- haskell/HVG/src/HVG/ContextState.hs
+++ haskell/HVG/src/HVG/ContextState.hs
@@ -1,6 +1,8 @@
 module HVG.ContextState where
 
 import Data.Monoid
+import qualified Data.Map as M
+import qualified Data.Set as S
 
 import HVG.Type
 
@@ -9,69 +11,140 @@ import HVG.Type
 --
 
 setTransform :: Matrix -> Builder ()
-setTransform val = Builder $ \bld ctx -> (bld, ctx {ctxTransform = val}, ())
+setTransform val = Builder $ \ctx bld -> (ctx {ctxTransform = val}, bld, ())
 getTransform :: Builder Matrix
 -- # I know that `ctxTransform ctx` works like `ctx.ctxTransform` in OOPL
 -- # so `ctx {ctxTransform = val}` must be the setter or sth..
-getTransform = Builder $ \bld ctx -> (bld, ctx, ctxTransform ctx)
+getTransform = Builder $ \ctx bld -> (ctx, bld, ctxTransform ctx)
 applyTransform :: Matrix -> Builder ()
-applyTransform val = Builder $ \bld ctx -> (bld, ctx {ctxTransform = val <> ctxTransform ctx}, ())
+applyTransform val = Builder $ \ctx bld -> (ctx {ctxTransform = val <> ctxTransform ctx}, bld, ())
 
 
 setFill :: Maybe String -> Builder ()
-setFill val = Builder $ \bld ctx -> (bld, ctx {ctxFill = val}, ())
+setFill val = Builder $ \ctx bld -> (ctx {ctxFill = val}, bld, ())
 getFill :: Builder (Maybe String)
-getFill = Builder $ \bld ctx -> (bld, ctx, ctxFill ctx)
+getFill = Builder $ \ctx bld -> (ctx, bld, ctxFill ctx)
 
 setStroke :: Maybe String -> Builder ()
-setStroke val = Builder $ \bld ctx -> (bld, ctx {ctxStroke = val}, ())
+setStroke val = Builder $ \ctx bld -> (ctx {ctxStroke = val}, bld, ())
 getStroke :: Builder (Maybe String)
-getStroke = Builder $ \bld ctx -> (bld, ctx, ctxStroke ctx)
+getStroke = Builder $ \ctx bld -> (ctx, bld, ctxStroke ctx)
 
 
 setLineWidth :: Double -> Builder ()
-setLineWidth val = Builder $ \bld ctx -> (bld, ctx {ctxLineWidth = val}, ())
+setLineWidth val = Builder $ \ctx bld -> (ctx {ctxLineWidth = val}, bld, ())
 getLineWidth :: Builder Double
-getLineWidth = Builder $ \bld ctx -> (bld, ctx, ctxLineWidth ctx)
+getLineWidth = Builder $ \ctx bld -> (ctx, bld, ctxLineWidth ctx)
 
 setLineCap :: LineCap -> Builder ()
-setLineCap val = Builder $ \bld ctx -> (bld, ctx {ctxLineCap = val}, ())
+setLineCap val = Builder $ \ctx bld -> (ctx {ctxLineCap = val}, bld, ())
 getLineCap :: Builder LineCap
-getLineCap = Builder $ \bld ctx -> (bld, ctx, ctxLineCap ctx)
+getLineCap = Builder $ \ctx bld -> (ctx, bld, ctxLineCap ctx)
 
 setLineJoin :: LineJoin -> Builder ()
-setLineJoin val = Builder $ \bld ctx -> (bld, ctx {ctxLineJoin = val}, ())
+setLineJoin val = Builder $ \ctx bld -> (ctx {ctxLineJoin = val}, bld, ())
 getLineJoin :: Builder LineJoin
-getLineJoin = Builder $ \bld ctx -> (bld, ctx, ctxLineJoin ctx)
+getLineJoin = Builder $ \ctx bld -> (ctx, bld, ctxLineJoin ctx)
 
 setMiterLimit :: Double -> Builder ()
-setMiterLimit val = Builder $ \bld ctx -> (bld, ctx {ctxMiterLimit = val}, ())
+setMiterLimit val = Builder $ \ctx bld -> (ctx {ctxMiterLimit = val}, bld, ())
 getMiterLimit :: Builder Double
-getMiterLimit = Builder $ \bld ctx -> (bld, ctx, ctxMiterLimit ctx)
+getMiterLimit = Builder $ \ctx bld -> (ctx, bld, ctxMiterLimit ctx)
 
 setLineDash :: [Double] -> Builder ()
-setLineDash val = Builder $ \bld ctx -> (bld, ctx {ctxLineDash = val}, ())
+setLineDash val = Builder $ \ctx bld -> (ctx {ctxLineDash = val}, bld, ())
 getLineDash :: Builder [Double]
-getLineDash = Builder $ \bld ctx -> (bld, ctx, ctxLineDash ctx)
+getLineDash = Builder $ \ctx bld -> (ctx, bld, ctxLineDash ctx)
 
 setLineDashOffset :: Double -> Builder ()
-setLineDashOffset val = Builder $ \bld ctx -> (bld, ctx {ctxLineDashOffset = val}, ())
+setLineDashOffset val = Builder $ \ctx bld -> (ctx {ctxLineDashOffset = val}, bld, ())
 getLineDashOffset :: Builder Double
-getLineDashOffset = Builder $ \bld ctx -> (bld, ctx, ctxLineDashOffset ctx)
+getLineDashOffset = Builder $ \ctx bld -> (ctx, bld, ctxLineDashOffset ctx)
 
 
 setTextAlign :: TextAlign -> Builder ()
-setTextAlign val = Builder $ \bld ctx -> (bld, ctx {ctxTextAlign = val}, ())
+setTextAlign val = Builder $ \ctx bld -> (ctx {ctxTextAlign = val}, bld, ())
 getTextAlign :: Builder TextAlign
-getTextAlign = Builder $ \bld ctx -> (bld, ctx, ctxTextAlign ctx)
+getTextAlign = Builder $ \ctx bld -> (ctx, bld, ctxTextAlign ctx)
 
 setTextBaseline :: TextBaseline -> Builder ()
-setTextBaseline val = Builder $ \bld ctx -> (bld, ctx {ctxTextBaseline = val}, ())
+setTextBaseline val = Builder $ \ctx bld -> (ctx {ctxTextBaseline = val}, bld, ())
 getTextBaseline :: Builder TextBaseline
-getTextBaseline = Builder $ \bld ctx -> (bld, ctx, ctxTextBaseline ctx)
+getTextBaseline = Builder $ \ctx bld -> (ctx, bld, ctxTextBaseline ctx)
 
 setFont :: String -> Builder ()
-setFont val = Builder $ \bld ctx -> (bld, ctx {ctxFont = val}, ())
+setFont val = Builder $ \ctx bld -> (ctx {ctxFont = val}, bld, ())
 getFont :: Builder String
-getFont = Builder $ \bld ctx -> (bld, ctx, ctxFont ctx)
+getFont = Builder $ \ctx bld -> (ctx, bld, ctxFont ctx)
 
+
+local :: Builder a -> Builder a
+local (Builder act) = Builder $ \ctx bld ->
+  let
+    (_, bld', a) = act ctx bld
+  in
+    (ctx, bld', a)
+
+name :: String -> Builder ()
+name nextName = Builder $ \ctx bld ->
+  ( ctx
+    { ctxNextDrawName = Just nextName
+    , ctxNextLinkName = Just nextName
+    }
+  , bld
+  , ()
+  )
+
+addDraw :: Draw -> Builder ()
+addDraw draw = Builder $ \ctx bld ->
+  case ctxNextDrawName ctx of
+    Nothing ->
+      (ctx, bld, ())
+
+    Just myName ->
+      let
+        bld' = bld
+          { bldNamedDraw = S.insert myName (bldNamedDraw bld)
+          , bldWaitDraw = M.delete myName (bldWaitDraw bld)
+          , bldDraw = bldDraw bld >> draw
+          }
+
+        bld'' = case M.lookup myName (bldWaitDraw bld) of
+          Nothing ->
+            bld'
+          Just (ContextedBuilder continue) ->
+            bld'' where (_, bld'', ()) = continue bld'
+
+      in
+        (ctx {ctxNextDrawName = Nothing}, bld'', ())
+
+addLink :: Link -> Builder ()
+addLink link = Builder $ \ctx bld ->
+  case ctxNextLinkName ctx of
+    Nothing ->
+      (ctx, bld, ())
+
+    Just myName ->
+      let
+        bld' = bld
+          { bldNamedLink = M.insert myName link (bldNamedLink bld)
+          , bldWaitLink = M.delete myName (bldWaitLink bld)
+          }
+
+        bld'' = case M.lookup myName (bldWaitLink bld) of
+          Nothing ->
+            bld'
+          Just (ContextedBuilder continue) ->
+            bld'' where (_, bld'', ()) = continue bld'
+
+      in
+        (ctx {ctxNextLinkName = Nothing}, bld'', ())
+
+queryDraw :: String -> Builder ()
+queryDraw drawName = undefined
+
+queryLink :: String -> Builder Link
+queryLink linkName = undefined
+
+--rotate :: Double -> Builder ()
+--rotate theta = applyTransform (
--- haskell/HVG/src/HVG/Type.hs
+++ haskell/HVG/src/HVG/Type.hs
@@ -150,34 +150,34 @@ initBuilderState = BuilderState
   , bldDraw = return ()
   }
 
-data Builder a = Builder (BuilderState -> ContextState -> (BuilderState, ContextState, a))
-data ContextedBuilder a = ContextedBuilder (BuilderState -> (BuilderState, ContextState, a))
+data Builder a = Builder (ContextState -> BuilderState -> (ContextState, BuilderState, a))
+data ContextedBuilder a = ContextedBuilder (BuilderState -> (ContextState, BuilderState, a))
 
 instance Functor Builder where
-  fmap f (Builder act) = Builder $ \bld ctx ->
+  fmap f (Builder act) = Builder $ \ctx bld ->
     let
-      (bld', ctx', a) = act bld ctx
+      (ctx', bld', a) = act ctx bld
     in
-      (bld', ctx', f a)
+      (ctx', bld', f a)
 
 instance Applicative Builder where
-  pure a = Builder $ \bld ctx -> (bld, ctx, a)
-  Builder fAct <*> Builder aAct = Builder $ \bld ctx ->
+  pure a = Builder $ \ctx bld -> (ctx, bld, a)
+  Builder fAct <*> Builder aAct = Builder $ \ctx bld ->
     let
-      (bld', ctx', f) = fAct bld ctx
-      (bld'', ctx'', a) = aAct bld' ctx'
+      (ctx', bld', f) = fAct ctx bld
+      (ctx'', bld'', a) = aAct ctx' bld'
     in
-      (bld'', ctx'', f a)
+      (ctx'', bld'', f a)
 
 instance Monad Builder where
-  Builder mAct >>= f = Builder $ \bld ctx ->
+  Builder mAct >>= f = Builder $ \ctx bld ->
     let
-      (bld', ctx', a) = mAct bld ctx
+      (ctx', bld', a) = mAct ctx bld
       Builder fAct = f a
     in
       -- XXX: `f a` 的結果， `Builder fAct` 中也有一個 context 吧？
       --      它是 `bld ctx` 還是 `bld' ctx'` ？或者都不是？
-      fAct bld' ctx'
+      fAct ctx' bld'
 
 data Matrix = Matrix
   Double Double Double
